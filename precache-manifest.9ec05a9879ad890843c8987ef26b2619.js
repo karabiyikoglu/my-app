1e772eb70969ba07eed44945e43791202a27fcac
ismail karabıyıkoğlu <ismail@ismails-MacBook-Pro.local>
2019-11-17 22:51:57 +0300

    Updates

---
+++ precache-manifest.9ec05a9879ad890843c8987ef26b2619.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262855067d4b1fda55b5e75b5b1365d5",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "4d58bc1ff759e9fb92cf",
+    "url": "/my-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "275a9cd351e9e7e11e3f",
+    "url": "/my-app/static/js/2.3d9806eb.chunk.js"
+  },
+  {
+    "revision": "4d58bc1ff759e9fb92cf",
+    "url": "/my-app/static/js/main.9bfa1a15.chunk.js"
+  },
+  {
+    "revision": "a65e55756f41965287cf",
+    "url": "/my-app/static/js/runtime-main.625c24d2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-app/static/media/logo.25bf045c.svg"
+  }
+]);
